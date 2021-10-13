d1953a0e3f1823f23f843b23b4af88da73e3c69b
SlavaNarbekov <vyacheslav.narbekov@gmail.com>
2021-10-13 11:42:44 +0300

    Lesson 4. part2

---
+++ tests/test_user_register.py
@@ -0,0 +1,36 @@
+import requests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+from datetime import datetime
+
+class TestUserRegister(BaseCase):
+    def setup(self):
+        base_part="learnqa"
+        domain="example.com"
+        random_part=datetime.now().strftime("%m%d%Y%H%M%S")
+        self.email=f"{base_part}{random_part}@{domain}"
+
+    def test_create_user_successfully(self):
+        data = {
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': self.email,
+            'password': '123'
+        }
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        Assertions.assert_code_status(response,200)
+        Assertions.assert_json_has_key(response,"id")
+
+    def test_create_user_with_existing_email(self):
+        email='vinkotov@example.com'
+        data={
+            'username':'learnqa',
+            'firstName':'learnqa',
+            'lastName':'learnqa',
+            'email':email,
+            'password':'123'
+        }
+        response = requests.post("https://playground.learnqa.ru/api/user/",data=data)
+        Assertions.assert_code_status(response,400)
+        assert response.content.decode("utf-8")==f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
